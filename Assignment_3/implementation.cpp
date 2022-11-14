c6bf2bdfb895072eb4d19936e461cf040e2d31c0
mattmac02 <mattmac02@hotmail.ca>
2022-11-13 19:46:28 -0500

    Code cleaning

--- Assignment_3/implementation.cpp
+++ Assignment_3/implementation.cpp
@@ -1,50 +1,181 @@
 #include "jumble.h"
+#include <time.h>
+#include <stdlib.h>
+#include <string>
 
-const charArrayPtr *JumblePuzzle::getJumble() const {
-
-    arrSize = getSize();
-
-
-    return nullptr;
+//Constructor
+JumblePuzzle::JumblePuzzle(const string& userInput, const string& diff) {
+    if (userInput.length() <= 2 || userInput.length() >= 11){
+        throw BadJumbleException("Not a valid input. Word length not allowed.");
+    }
+    if (diff != "easy" && diff != "medium" && diff != "hard"){
+        throw BadJumbleException("Not a valid game mode.");
+    }
+    this->diff = diff;
+    this->userInput = userInput;
+    this->puzzSize = makeSize();
+    this->puzz = createPuzz();
 }
 
+//Making the Grid Size Based on Game Mode
 const int& JumblePuzzle::makeSize() {
     if (diff == "easy"){
-    return userInput.length() * 2;
-    printf("heyo");
+        return userInput.length() * 2;
     }
     else if (diff == "medium"){
         return userInput.length() * 3;
     }
-    else if (diff == "hard" || diff == "extreme"){
+    else if (diff == "hard"){
         return userInput.length() * 4;
     }
     return 0;
 }
 
-int JumblePuzzle::getRowPos() {
-    return 0;
-}
-
-int JumblePuzzle::getColPos() {
-    return 0;
-}
-
-char JumblePuzzle::getDirection() {
-    return 0;
-}
-
-JumblePuzzle::JumblePuzzle(const string& userIn, const string& diffi) {
-    if (userIn.length() <= 2 && userIn >= 11){
-        printf("Not a valid input");
-        exit();
+//Create the Puzzle
+charArrayPtr *JumblePuzzle::createPuzz() {
+    charArrayPtr* filledPuzz = randPuzz(puzzSize);
+    int i = 0;
+    int pos = 0;
+    while (i == 0){
+        direc = direcArr[rand() % 4];   //Choosing a random direction out of the 4 possible choices
+        colP = rand() % puzzSize;   //Randomly choosing the column to insert to
+        rowP = rand() % puzzSize;   //Randomly choosing the row to insert to
+        if (direc == 'n'){
+            while (rowP >= 0 && pos < userInput.length()){
+                filledPuzz[rowP--][colP] = userInput[pos++];    //Insert the word letter by letter in the corresponding direction
+            }
+            if (pos == userInput.length()){
+                i = 1;
+            }
+        }
+        else if (direc == 'e'){
+            while (colP < puzzSize && pos < userInput.length()){
+                filledPuzz[rowP][colP++] = userInput[pos++];
+            }
+            if (pos == userInput.length()){
+                i = 1;
+            }
+        }
+        else if (direc == 's'){
+            while (rowP < puzzSize && pos < userInput.length()){
+                filledPuzz[rowP++][colP] = userInput[pos++];
+            }
+            if (pos == userInput.length()){
+                i = 1;
+            }
+        }
+        else if (direc == 'w'){
+            while (colP >= 0 && pos < userInput.length()){
+                filledPuzz[rowP][colP--] = userInput[pos++];
+            }
+            if (pos == userInput.length()){
+                i = 1;
+            }
+        }
     }
-    if (diffi != "easy" || diffi != "medium" || diffi != "hard" || diffi != "extreme" ||){
-        printf("Not a valid game mode");
-        exit();
+    return filledPuzz;
+}
+
+//Initializing the random puzzle being generated and ensuring all letters are in the alphabet and lowercase
+charArrayPtr *JumblePuzzle::randPuzz(const int& size) {
+    charArrayPtr * randPuzz = new charArrayPtr[size];
+    srand(time(NULL));
+    int n;
+    for (int i = 0; i < size; i++){
+        randPuzz[i] = new char[size];
+        for (int k = 0; k < size; k++){
+            n = rand() % 26;
+            randPuzz[i][k] = 'a' + n;
+        }
     }
-    this->userIn = userIn;
-    this->diffi = diffi;
-    this->size = getSize();
-    this->puzzCreate = getJumble();
+    return randPuzz;
+}
+
+//Prevent aliasing
+charArrayPtr *JumblePuzzle::getJumble() const {
+    charArrayPtr* tempPuzzle = new charArrayPtr[puzzSize];
+    for (int i = 0; i < puzzSize; i++) {
+        tempPuzzle[i] = new char[puzzSize];
+        for (int j = 0; j < puzzSize; j++) {
+            tempPuzzle[i][j] = puzz[i][j];
+        }
+    }
+    return tempPuzzle;
+}
+
+//Destructor
+JumblePuzzle::~JumblePuzzle() {
+    for (int i = 0; i < this->puzzSize; i++) {
+        delete[] puzz[i];
+    }
+    delete[] puzz;
+    puzz = nullptr;
+}
+
+//Operator overloading
+JumblePuzzle& JumblePuzzle::operator=(const JumblePuzzle& right) {
+    if (this != &right) {
+        for (int i = 0; i < puzzSize; i++) {
+            delete[] puzz[i];
+        }
+        delete[] puzz;
+        puzzSize = right.getSize();
+        rowP = right.getRowPos();
+        colP = right.getColPos();
+        direc = right.getDirection();
+
+        charArrayPtr* currPuzz = right.getJumble();
+        puzz = new charArrayPtr[puzzSize];
+
+        for (int i = 0; i < puzzSize; i++) {
+            puzz[i] = new char[puzzSize];
+            for (int k = 0; k < puzzSize; k++) {
+                puzz[i][k] = currPuzz[i][k];
+            }
+        }
+    }
+    return *this;
+}
+
+//Exception
+BadJumbleException::BadJumbleException(const string& m) : errorMessage(m) {}
+string& BadJumbleException::what() { return errorMessage; }
+
+
+//Copy-Constructor
+JumblePuzzle::JumblePuzzle(const JumblePuzzle &jumb) {
+    rowP = jumb.getRowPos();
+    colP = jumb.getColPos();
+    direc = jumb.getDirection();
+    puzzSize = jumb.getSize();
+
+    charArrayPtr* currPuzz = jumb.getJumble();
+    puzz = new charArrayPtr[this->puzzSize];
+
+    for (int i = 0; i < puzzSize; i++) {
+        puzz[i] = new char[puzzSize];
+        for (int j = 0; j < puzzSize; j++) {
+            puzz[i][j] = currPuzz[i][j];
+        }
+    }
+}
+
+//Row Position Getter
+int JumblePuzzle::getRowPos() const{
+    return this->rowP;
+}
+
+//Grid Size Getter
+int JumblePuzzle::getSize() const{
+    return this->puzzSize;
+}
+
+//Column Position Getter
+int JumblePuzzle::getColPos() const{
+    return this->colP;
+}
+
+//Direction Getter
+char JumblePuzzle::getDirection() const{
+    return this->direc;
 }
--- Assignment_3/jumble.h
+++ Assignment_3/jumble.h
@@ -12,26 +12,35 @@ typedef char* charArrayPtr;
 
 class JumblePuzzle {
 public:
+    //Variable Declaration
     string userInput;
     string diff;
+    int size;
+    int puzzSize;
+    int rowP;
+    int colP;
+    char direcArr[4] = {'n', 'e', 's', 'w'};    //Direction array
+    char direc;
 
-    JumblePuzzle(const string& userI, const string& diffi);
+    JumblePuzzle& operator=(const JumblePuzzle&); //Assignment Operator
 
-//    JumblePuzzle(const JumblePuzzle& jp){
-//        userInput = jp.userInput;
-//        diff = jp.diff;
-//    }
+    JumblePuzzle(const string& toHide, const string& difficulty);   //Constructor
 
-    charArrayPtr* getJumble() const;
-    const int& makeSize();
-    int getSize();
-    int getRowPos();
-    int getColPos();
-    char getDirection();
+    JumblePuzzle(const JumblePuzzle&);  //Copy-Constructor
+
+    ~JumblePuzzle();    //Destructor
+
+    charArrayPtr* puzz = nullptr;
+    charArrayPtr* createPuzz(); //Puzzle creation
+    charArrayPtr* randPuzz(const int& size);    //Random letter creation and insertion
+    charArrayPtr* getJumble() const;    //Aliasing prevention
+
+    const int& makeSize();  //Making the size of the grid
+    int getSize() const;    //Getting the size of the grid
+    int getRowPos() const;  //Getting the row position
+    int getColPos() const;  //Getting the column position
+    char getDirection() const;  //Getting the direction
 };
-
-
-
 
 //Exception Class
 class BadJumbleException : public exception {
