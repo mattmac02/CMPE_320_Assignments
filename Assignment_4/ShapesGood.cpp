c6bb335f8b54ac3db94900859d60c7f1cf674c27
mattmac02 <mattmac02@hotmail.ca>
2022-12-03 17:46:02 -0500

    Commenting

--- Assignment_4/ShapesGood.cpp
+++ Assignment_4/ShapesGood.cpp
@@ -2,4 +2,86 @@
 // Created by Matthew MacEachern on 12/3/22.
 //
 
+#include <iostream>
+#include <string>
 #include "ShapesGood.h"
+
+using namespace std;
+
+// Shape Functions
+// Destructor
+Shape::~Shape() {}
+
+// Constructor
+Shape::Shape(int l, int w) {
+    this->length = l;
+    this->width = w;
+}
+
+// Length Accessor
+int Shape::getLength() {
+    return this->length;
+}
+
+// Width Accessor
+int Shape::getWidth() {
+    return this->width;
+}
+
+// Square Functions
+// Destructor
+Square::~Square() {}
+
+// Constructor
+Square::Square(int l, int w, std::string outCol) {
+    outlineCol = outCol;
+}
+
+// Outside Colour Accessor
+std::string Square::getOutCol() {
+    return this->outlineCol;
+}
+
+// Drawing Statement
+void Square::draw() {
+    cout << "Drawing a " + getOutCol() + " square\n";
+}
+
+// Filled Square Functions
+// Constructor
+FilledSquare::FilledSquare(int l, int w, std::string outCol, std::string fillCol) {
+    fillColour = fillCol;
+}
+
+// Destructor
+FilledSquare::~FilledSquare() {}
+
+// Fill Colour Accessor
+std::string FilledSquare::getFillCol() {
+    return this->fillColour;
+}
+
+// Drawing Statement
+void FilledSquare::draw() {
+    cout << "Drawing a square with a " + getOutCol()  + "border and a " + getFillCol() + "fill";
+}
+
+// Filled Text Square Functions
+// Destructor
+FilledTextSquare::~FilledTextSquare() {}
+
+// Constructor
+FilledTextSquare::FilledTextSquare(int l, int w, std::string outCol, std::string fillCol, std::string textMessage) {
+    textMess = textMessage;
+}
+
+// Accessor
+void FilledTextSquare::getTextMessage() {
+    return this->textMess;
+}
+
+// Drawing Statement
+void FilledTextSquare::draw() {
+    cout << "Drawing a square with a " + getOutCol()  + "border and a " + getFillCol() + "fill containing the text " + getTextMessage();
+}
+
--- Assignment_4/ShapesGood.h
+++ Assignment_4/ShapesGood.h
@@ -6,8 +6,45 @@
 #define ASSIGNMENT_4_SHAPESGOOD_H
 
 
-class ShapesGood {
+class Shape {
+public:
+    virtual ~Shape();   // Destructor
+    Shape(int l, int w);
+    int getLength();
+    int getWidth();
+private:
+    int length;
+    int width;
+};
 
+class Square : public Shape {
+public:
+    virtual ~Square();  // Destructor
+    void draw();        // Drawing Statement
+    Square(int l, int w, string outCol);    // Constructor
+    string getOutCol(); // Outline Colour Accessor
+private:
+    string outCol;
+};
+
+class FilledSquare : public Square {
+    FilledSquare(int l, int w, string outCol, string fillCol);  // Constructor
+    virtual ~FilledSquare();    // Destructor
+    string getFillCol();        // Fill Colour Accessor
+    void draw();                // Drawing Statement
+private:
+    string fillCol;
+};
+
+class FilledTextSquare : public Square {
+public:
+    FilledTextSquare(int l, int w, string outCol, string fillCol, string textMessage);
+    virtual ~FilledTextSquare();
+    void draw();
+    void getTextMessage();
+    void drawText(string);
+private:
+    string textMessage;
 };
 
 
