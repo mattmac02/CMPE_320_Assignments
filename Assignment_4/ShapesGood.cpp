b1fe4f79d502e04596864a40267b264af79e7fcb
mattmac02 <mattmac02@hotmail.ca>
2022-12-04 14:56:21 -0500

    Final fixes

--- Assignment_4/ShapesGood.cpp
+++ Assignment_4/ShapesGood.cpp
@@ -18,6 +18,11 @@ Shape::Shape(int l, int w) {
     this->width = w;
 }
 
+// Drawing Statement
+void Shape::draw() {
+    cout << "\nDrawing the default class where no shape will be drawn\n";
+}
+
 // Length Accessor
 int Shape::getLength() {
     return this->length;
@@ -28,42 +33,63 @@ int Shape::getWidth() {
     return this->width;
 }
 
+// Label Functions
+// Constructor
+Label::Label(string lab) {
+    label = lab;
+}
+
+// Accessor
+string Label::getLabel() {
+    return this->label;
+}
+
+// Destructor
+Label::~Label() {}
+
+//Fill Functions
+// Destructor
+Fill::~Fill() {}
+
+// Fill Colour Accessor
+string Fill::getFillCol() {
+    return this->fillColour;
+}
+
+// Constructor
+Fill::Fill(string fillCol) {
+    fillColour = fillCol;
+}
+
 // Square Functions
 // Destructor
 Square::~Square() {}
 
 // Constructor
-Square::Square(int l, int w, std::string outCol) {
-    outlineCol = outCol;
+Square::Square(int l, int w, std::string outlineCol) : Shape(l, w){
+    outCol = outlineCol;
 }
 
 // Outside Colour Accessor
 std::string Square::getOutCol() {
-    return this->outlineCol;
+    return this->outCol;
 }
 
 // Drawing Statement
 void Square::draw() {
-    cout << "Drawing a " + getOutCol() + " square\n";
+    cout << "\nDrawing a " + getOutCol() + " square\n";
 }
 
 // Filled Square Functions
 // Constructor
-FilledSquare::FilledSquare(int l, int w, std::string outCol, std::string fillCol) {
-    fillColour = fillCol;
-}
+FilledSquare::FilledSquare(int l, int w, std::string outCol, std::string fillColour) : Square(l, w, outCol), Fill(fillColour) {}
 
 // Destructor
 FilledSquare::~FilledSquare() {}
 
-// Fill Colour Accessor
-std::string FilledSquare::getFillCol() {
-    return this->fillColour;
-}
-
 // Drawing Statement
 void FilledSquare::draw() {
-    cout << "Drawing a square with a " + getOutCol()  + "border and a " + getFillCol() + "fill";
+    cout << "\nDrawing a square with a " + getOutCol()  + " border and a " + getFillCol() + " fill\n";
 }
 
 // Filled Text Square Functions
@@ -71,17 +97,59 @@ void FilledSquare::draw() {
 FilledTextSquare::~FilledTextSquare() {}
 
 // Constructor
-FilledTextSquare::FilledTextSquare(int l, int w, std::string outCol, std::string fillCol, std::string textMessage) {
-    textMess = textMessage;
-}
-
-// Accessor
-void FilledTextSquare::getTextMessage() {
-    return this->textMess;
-}
+FilledTextSquare::FilledTextSquare(int l, int w, std::string outCol, std::string fillCol, std::string label) : Square(l, w, outCol), Fill(fillCol),Label(label) {}
 
 // Drawing Statement
 void FilledTextSquare::draw() {
-    cout << "Drawing a square with a " + getOutCol()  + "border and a " + getFillCol() + "fill containing the text " + getTextMessage();
+    cout << "\nDrawing a square with a " + getOutCol()  + " border and a " + getFillCol() + " fill containing the text \"" + getLabel() + "\"\n";
 }
 
+// Circle Functions
+// Constructor
+Circle::Circle(int l, int w, std::string outlineCol) : Shape(l, w) {
+    outCol = outlineCol;
+}
+
+// Outline Colour Accessor
+std::string Circle::getOutCol() {
+    return this->outCol;
+}
+
+// Destructor
+Circle::~Circle() {}
+
+// Drawing Statement
+void Circle::draw() {
+    cout << "\nDrawing a circle with a " + getOutCol() + " outline\n";
+}
+
+// Filled Circle Functions
+// Constructor
+FilledCircle::FilledCircle(int l, int w, std::string outCol, std::string fillColour) : Circle(l, w, outCol), Fill(fillColour) {}
+
+// Drawing Statement
+void FilledCircle::draw() {
+    cout << "\nDrawing a circle with a " + getOutCol() + " outline and a " + getFillCol() + " fill\n";
+}
+
+// Destructor
+FilledCircle::~FilledCircle() {}
+
+// Arc Functions
+// Accessor
+std::string Arc::getOutCol() {
+    return this->outCol;
+}
+
+// Constructor
+Arc::Arc(int l, int w, std::string outlineCol) : Shape(l, w) {
+    outCol = outlineCol;
+}
+
+// Destructor
+Arc::~Arc() {}
+
+// Drawing Statement
+void Arc::draw() {
+    cout << "\nDrawing an arc with a " + getOutCol() + " outline\n";
+}
--- Assignment_4/ShapesGood.h
+++ Assignment_4/ShapesGood.h
@@ -4,48 +4,88 @@
 
 #ifndef ASSIGNMENT_4_SHAPESGOOD_H
 #define ASSIGNMENT_4_SHAPESGOOD_H
-
+#include <string>
+using namespace std;
 
 class Shape {
 public:
-    virtual ~Shape();   // Destructor
-    Shape(int l, int w);
-    int getLength();
-    int getWidth();
+    virtual ~Shape();       // Destructor
+    Shape(int l, int w);    // Constructor
+    virtual void draw();    // Drawing Statement
+    int getLength();        // Length Accessor
+    int getWidth();         // Width Accessor
 private:
     int length;
     int width;
 };
 
+class Label {
+public:
+    Label(string lab);  // Constructor
+    virtual ~Label();   // Destructor
+    string getLabel();  // Label Accessor
+private:
+    string label;
+};
+
+class Fill {
+public:
+    Fill(string fillColour);    // Constructor
+    virtual ~Fill();            // Destructor
+    string getFillCol();        // Fill Colour Accessor
+private:
+    string fillColour;
+};
+
 class Square : public Shape {
 public:
-    virtual ~Square();  // Destructor
-    void draw();        // Drawing Statement
+    virtual ~Square();                      // Destructor
+    virtual void draw();                    // Drawing Statement
     Square(int l, int w, string outCol);    // Constructor
-    string getOutCol(); // Outline Colour Accessor
+    string getOutCol();                     // Outline Colour Accessor
 private:
     string outCol;
 };
 
-class FilledSquare : public Square {
-    FilledSquare(int l, int w, string outCol, string fillCol);  // Constructor
-    virtual ~FilledSquare();    // Destructor
-    string getFillCol();        // Fill Colour Accessor
-    void draw();                // Drawing Statement
-private:
-    string fillCol;
-};
-
-class FilledTextSquare : public Square {
+class FilledSquare : public Square, public Fill {
 public:
-    FilledTextSquare(int l, int w, string outCol, string fillCol, string textMessage);
-    virtual ~FilledTextSquare();
-    void draw();
-    void getTextMessage();
-    void drawText(string);
-private:
-    string textMessage;
+    FilledSquare(int l, int w, string outCol, string fillCol);  // Constructor
+    virtual ~FilledSquare();                                    // Destructor
+    virtual void draw();                                        // Drawing Statement
 };
 
+class FilledTextSquare : public Square, public Fill, public Label {
+public:
+    FilledTextSquare(int l, int w, string outCol, string fillCol, string lab);  // Constructor
+    virtual ~FilledTextSquare();                                                // Destructor
+    virtual void draw();                                                        // Drawing Statement
+};
+
+class Circle : public Shape {
+public:
+    Circle(int l, int w, string outCol);    // Constructor
+    virtual ~Circle();                      // Destructor
+    virtual void draw();                    // Drawing Statement
+    string getOutCol();                     // Outline Colour Accessor
+private:
+    string outCol;
+};
+
+class FilledCircle : public Circle, public Fill {
+public:
+    FilledCircle(int l, int w, string outCol, string fillCol);  // Constructor
+    virtual void draw();                                        // Drawing Statement
+    virtual ~FilledCircle();                                    // Destructor
+};
+
+class Arc : public Shape {
+public:
+    Arc(int l, int w, string outCol);   // Constructor
+    virtual void draw();                // Drawing Statement
+    string getOutCol();                 // Outline Colour Accessor
+    virtual ~Arc();                     // Destructor
+private:
+    string outCol;
+};
 
 #endif //ASSIGNMENT_4_SHAPESGOOD_H
